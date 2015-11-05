f9c9e466185ef69f7a56fa9cf7d64982767b3742
Drew Fyock <fyockm@gmail.com>
2015-11-05 12:34:14 -0500

    Allow options to be passed to hawk authenticate
    Main use case is with Heroku, where the url is `https` but that's
    translated to a different port internally. See added test for example.
    
    Also:
    - update all deps
    - DRY call to hawk authenticate
    - add test for options
    - apply consistent formatting
    
     _Note:_
    `hawk.uri.authenticate` === `hawk.server.authenticateBewit`
    per https://github.com/hueniverse/hawk/blob/master/lib/index.js#L11-L14

--- test/bewit.tests.js
+++ test/bewit.tests.js
@@ -1,5 +1,6 @@
 var HawkStrategy = require('../lib/strategy'),
-  Hawk = require('hawk');
+  Hawk = require('hawk'),
+  should = require('should');
 
 var credentials = {
   key: 'abcd',
@@ -8,8 +9,8 @@ var credentials = {
   id: 'dasd123'
 };
 
-var strategy = new HawkStrategy({bewit: true}, function(id, done) {
-  if(id === credentials.id) return done(null, credentials);
+var strategy = new HawkStrategy({ bewit: true }, function(id, done) {
+  if (id === credentials.id) return done(null, credentials);
   return done(null, null);
 });
 
@@ -17,13 +18,16 @@ var strategy = new HawkStrategy({bewit: true}, function(id, done) {
 describe('passport-hawk with bewit', function() {
 
   it('can authenticate a request with a correct header', function(testDone) {
-    var bewit = Hawk.uri.getBewit('http://example.com:8080/resource/4?filter=a', { credentials: credentials, ttlSec: 60 * 5});
+    var bewit = Hawk.uri.getBewit('http://example.com:8080/resource/4?filter=a', {
+      credentials: credentials,
+      ttlSec: 60 * 5
+    });
     var req = {
       headers: {
         host: 'example.com:8080'
       },
       method: 'GET',
-      url: '/resource/4?filter=a&bewit=' + bewit  
+      url: '/resource/4?filter=a&bewit=' + bewit
     };
 
     strategy.success = function(user) {
@@ -38,15 +42,18 @@ describe('passport-hawk with bewit', function() {
   });
 
   it('should properly fail with correct challenge code when using different url', function(testDone) {
-    var bewit = Hawk.uri.getBewit('http://example.com:8080/resource/4?filter=a' + bewit, { credentials: credentials, ttlSec: 60 * 5});
+    var bewit = Hawk.uri.getBewit('http://example.com:8080/resource/4?filter=a' + bewit, {
+      credentials: credentials,
+      ttlSec: 60 * 5
+    });
     var req = {
       headers: {
         host: 'example.com:8080'
       },
       method: 'GET',
-      url: '/resource/4?filter=a&bewit=' + bewit  
+      url: '/resource/4?filter=a&bewit=' + bewit
     };
-    strategy.error = function(challenge) {      
+    strategy.error = function(challenge) {
       challenge.message.should.eql('Bad mac');
       testDone();
     };
@@ -54,18 +61,21 @@ describe('passport-hawk with bewit', function() {
   });
 
   it('should call done with false when the id doesnt exist', function(testDone) {
-    var bewit = Hawk.uri.getBewit('http://example.com:8080/foobar', { credentials: {
+    var bewit = Hawk.uri.getBewit('http://example.com:8080/foobar', {
+      credentials: {
         id: '321321',
         key: 'dsa',
         algorithm: 'sha256'
-      }, ttlSec: 60 * 5});
-  
+      },
+      ttlSec: 60 * 5
+    });
+
     var req = {
       headers: {
         host: 'example.com:8080'
       },
       method: 'GET',
-      url: '/resource/4?filter=a&bewit=' + bewit  
+      url: '/resource/4?filter=a&bewit=' + bewit
     };
 
     strategy.error = function(challenge) {
@@ -76,7 +86,7 @@ describe('passport-hawk with bewit', function() {
   });
 
   it('should call fail when url doesnt have a bewit', function(testDone) {
-  
+
     var req = {
       headers: {
         host: 'example.com:8080'
